776a56db58ef81ea292018d3de4e7b9010b9551b
~sig <david@crap.solutions>
2023-12-13 06:51:21 +0100

    ADD: day 13

--- aoc-2023/src/aoc_2023/day_13.clj
+++ aoc-2023/src/aoc_2023/day_13.clj
@@ -7,15 +7,49 @@
 
 (defn parse-input
   [filename]
-  (let [entries (string/split (slurp filename) #"\n")]))
+  (let [patterns (string/split (slurp filename) #"\n\n")]
+    (map (fn [entry] (map #(apply vector %) (string/split entry #"\n")))
+         patterns)))
+
+(defn check-mirror
+  [pattern]
+  (reduce (fn [_ idx]
+            (let [start (reverse (take idx pattern))
+                  end (drop idx pattern)]
+              (if (every? identity (map #(= %1 %2) start end))
+                (reduced idx)
+                0)))
+          0
+          (range 1 (count pattern))))
 
 (defn part-one
   ([] (part-one input-file-path))
-  ([filename]))
+  ([filename]
+   (let [patterns (parse-input filename)]
+     (+ (* 100 (apply + (map check-mirror patterns)))
+        (apply + (map #(check-mirror (apply map vector %)) patterns))))))
+
+(defn get-diff-cnt
+  [a b]
+  (count (filter false? (map #(= %1 %2) a b))))
+
+(defn check-broken
+  [pattern]
+  (reduce (fn [_ idx]
+            (let [start (reverse (take idx pattern))
+                  end (drop idx pattern)]
+              (if (= 1 (apply + (map #(get-diff-cnt %1 %2) start end)))
+                (reduced idx)
+                0)))
+          0
+          (range 1 (count pattern))))
 
 (defn part-two
   ([] (part-two input-file-path))
-  ([filename]))
+  ([filename]
+   (let [patterns (parse-input filename)]
+     (+ (* 100 (apply + (map check-broken patterns)))
+        (apply + (map #(check-broken (apply map vector %)) patterns))))))
 
 (defn run 
   []
