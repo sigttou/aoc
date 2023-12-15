9f7508042d8908b14ee284d3ddb1a8a9c31376c5
~sig <david@crap.solutions>
2023-12-15 18:21:32 +0100

    ADD: day 15

--- aoc-2023/src/aoc_2023/day_15.clj
+++ aoc-2023/src/aoc_2023/day_15.clj
@@ -7,15 +7,59 @@
 
 (defn parse-input
   [filename]
-  (let [entries (string/split (slurp filename) #"\n")]))
+  (let [entries (string/split (slurp filename) #",")]
+    entries))
+
+(defn hash-str
+  [in]
+  (reduce (fn [val entry]
+            (mod (* 17 (+ val (int entry))) 256))
+          0
+          in))
 
 (defn part-one
   ([] (part-one input-file-path))
-  ([filename]))
+  ([filename]
+   (reduce + (map hash-str (parse-input filename)))))
+
+(defn lens-to-box
+  [boxes lens]
+  (let [[label val] (string/split lens #"=|-")
+        hash (hash-str label)]
+    (if val
+      (update boxes hash (fn [box]
+                           (let [idx (.indexOf (map first box) label)]
+                             (if (= -1 idx)
+                               (apply vector (conj box [label val]))
+                               (assoc box idx [label val])))))
+      (update boxes hash (fn [box]
+                           (apply vector (remove #(= label (first %)) box)))))))
+
+(defn fill-boxes
+  [lenses]
+  (let [boxes (apply vector (repeat 256 []))]
+    (reduce #(lens-to-box %1 %2)
+            boxes
+            lenses)))
+
+(defn score-boxes
+  [boxes]
+  (reduce (fn [score box-idx]
+            (+ score (reduce (fn [val [lens-idx entry]]
+                               (+ val (* (inc box-idx) (inc lens-idx)
+                                         (Integer. (second entry)))))
+                             0
+                             (keep-indexed #(identity %&)(get boxes box-idx)))))
+          0
+          (range (count boxes))))
 
 (defn part-two
   ([] (part-two input-file-path))
-  ([filename]))
+  ([filename]
+   (let [lenses (parse-input filename)]
+     (->> lenses
+          fill-boxes
+          score-boxes))))
 
 (defn run 
   []
