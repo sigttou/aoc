e9b22e88bb1956d053bb7dd8b8a548399315674a
~sig <david@crap.solutions>
2023-12-19 10:19:25 +0100

    IMP: remove eval

--- aoc-2023/src/aoc_2023/day_19.clj
+++ aoc-2023/src/aoc_2023/day_19.clj
@@ -43,7 +43,7 @@
   [wfs name shape]
   (some (fn [op]
           (if (:op op)
-            (if ((eval (read-string (str (:op op))))
+            (if ((partial (get {\< <} (:op op) >))
                  (get shape (:target op)) (:val op))
               (if (get wfs (:des op))
                 (apply-wf wfs (:des op) shape)
