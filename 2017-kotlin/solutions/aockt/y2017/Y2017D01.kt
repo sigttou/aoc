a0d7a6a9bbf4729ecff685320452e6292b9ba358
~sig <david@crap.solutions>
2025-01-26 07:45:27 +0100

    IMP: code quality

--- 2017-kotlin/solutions/aockt/y2017/Y2017D01.kt
+++ 2017-kotlin/solutions/aockt/y2017/Y2017D01.kt
@@ -6,7 +6,7 @@ import io.github.jadarma.aockt.core.Solution
 object Y2017D01 : Solution {
 
     override fun partOne(input: String) : Int {
-        var ret: Int = 0
+        var ret = 0
 
         for (i in  input.indices) {
             val c = input[i]
@@ -17,15 +17,15 @@ object Y2017D01 : Solution {
     }
 
     override fun partTwo(input: String) : Int {
-        var ret: Int = 0
+        var ret = 0
 
         for (i in input.indices) {
             val c = input[i]
-            val nxtidx = input.length / 2
-            val nxt = if (i + nxtidx >= input.length) {
-                input[i + nxtidx - input.length]
+            val nxtIndex = input.length / 2
+            val nxt = if (i + nxtIndex >= input.length) {
+                input[i + nxtIndex - input.length]
             } else {
-                input[i + nxtidx]
+                input[i + nxtIndex]
             }
             if (c == nxt) ret += c.toString().toInt()
         }
