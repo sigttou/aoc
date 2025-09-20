1e9c70540dd81df2ca60661fecf2528d65aecb57
~sig <david@crap.solutions>
2025-09-20 07:37:44 +0200

    ADD: 2021 template

---
+++ 2021-ruby/spec/spec_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Dir[File.join(File.dirname(__FILE__), "..", "challenges", "shared", "**", "*.rb")].each do |file|
+  require file
+end
+
+Dir[File.join(File.dirname(__FILE__), "..", "challenges", "20*", "**", "*.rb")].each do |file|
+  require file
+end
+
+RSpec.configure do |config|
+end
