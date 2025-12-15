c418b71fa650a4d08cc47f535ba9db1a8a2b53d7
~sig <david@crap.solutions>
2025-12-15 16:24:26 +0100

    DEL: 2022-python

--- 2022-python/solutions/utils/example.py
+++
@@ -1,7 +0,0 @@
-# add whatever utilities you'll find useful across multiple solutions
-# import them in a solution using:
-# from ...utils.example import add
-
-
-def add(a: int, b: int):
-    return a + b
